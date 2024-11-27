b0f74dd64b0b9d8bedf088062df46f74273ce5f4
Osvaldo Mesa <d3v.ozzy@gmail.com>
2024-11-27 18:13:26 +0800

    add jte rendering engine

--- src/main/java/com/mycompany/app/Controller/AppController.java
+++ src/main/java/com/mycompany/app/Controller/AppController.java
@@ -2,13 +2,20 @@ package com.mycompany.app.Controller;
 
 import io.javalin.Javalin;
 import io.javalin.http.Context;
+import io.javalin.rendering.template.JavalinJte;
 
 public class AppController {
   public Javalin startAPI() {
-    Javalin app = Javalin.create();
-    app.get("/", ctx -> {
-      ctx.result("this works");
+
+    Javalin app = Javalin.create(config -> {
+      config.fileRenderer(new JavalinJte());
     });
+    app.get("/", this::test);
+
     return app;
+  }
+
+  private void test(Context ctx) {
+    ctx.render("test.jte");
   }
 }
