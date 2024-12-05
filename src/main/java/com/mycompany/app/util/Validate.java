f35476ba1e4e6653c9b315f3a3dc79af5bf80f16
Osvaldo Mesa <d3v.ozzy@gmail.com>
2024-12-05 15:18:20 -0800

    add test on void input days, update variable names and modifiers

--- src/main/java/com/mycompany/app/util/Validate.java
+++ src/main/java/com/mycompany/app/util/Validate.java
@@ -6,44 +6,39 @@ import com.mycompany.app.model.ValidateResultObject;
 import java.util.*;
 
 public class Validate {
-  public ValidateResultObject returnObj = new ValidateResultObject();
-  public InputDate date;
-  public String partialVoidMessage = "Must be a valid ";
-  public String pastMessage = "Must be in the past";
-  public String requiredMessage = "This field is required";
+  private InputDate date;
+  private String partialVoidMessage = "Must be a valid ";
+  private String pastMessage = "Must be in the past";
+  private String requiredMessage = "This field is required";
 
   public ValidateResultObject day(String day, String month, String year) {
-    // TODO: trim incoming strings before they hit methods
+    ValidateResultObject dayResultReturnObj = new ValidateResultObject();
     if (day == "") {
-      returnObj.setpassing(false);
-      returnObj.setMessage(requiredMessage);
-      return returnObj;
+      dayResultReturnObj.setpassing(false);
+      dayResultReturnObj.setMessage(requiredMessage);
+      return dayResultReturnObj;
     }
     ;
     InputDate date = new InputDate(Integer.parseInt(day), Integer.parseInt(month), Integer.parseInt(year));
 
     Calendar dateObj = Calendar.getInstance();
     dateObj.set(date.getYear() + 1900, date.getMonth() - 1, date.getDay());
-    if (date.getDay() != 0) {
-      returnObj.setMessage(partialVoidMessage + "day");
-    }
-    ;
 
     if (dateObj.get(Calendar.YEAR) - 1900 == Calendar.getInstance().get(Calendar.YEAR) - 1900 &&
         dateObj.get(Calendar.MONTH) == Calendar.getInstance().get(Calendar.MONTH) &&
         dateObj.getTime().getTime() > Calendar.getInstance().getTime().getTime()) {
-      returnObj.message = pastMessage;
+      dayResultReturnObj.message = pastMessage;
     }
     ;
 
     if (dateObj.get(Calendar.DAY_OF_MONTH) != date.getDay()) {
-      returnObj.setMessage(partialVoidMessage + "day");
+      dayResultReturnObj.setMessage(partialVoidMessage + "day");
     }
     ;
 
-    if (returnObj.message.length() > 1)
-      returnObj.setpassing(false);
-    return returnObj;
+    if (dayResultReturnObj.message.length() > 1)
+      dayResultReturnObj.setpassing(false);
+    return dayResultReturnObj;
   }
 
 }
--- src/test/java/com/mycompany/app/util/ValidateTest.java
+++ src/test/java/com/mycompany/app/util/ValidateTest.java
@@ -9,12 +9,54 @@ public class ValidateTest {
     Validate validate = new Validate();
 
     @Test
-    public void day_passEmptyString() {
-        String day = " ";
+    public void day_passInEmptyString() {
+        String day = "";
         String month = "2";
         String year = "2023";
 
         ValidateResultObject expectedObj = new ValidateResultObject(false, "This field is required");
+
+        ValidateResultObject returnObj = validate.day(day, month, year);
+
+        assertEquals(expectedObj.ispassing(), returnObj.ispassing());
+        assertEquals(expectedObj.getMessage(), returnObj.getMessage());
+    }
+
+    @Test
+    public void day_passInZero() {
+        String day = "0";
+        String month = "2";
+        String year = "2023";
+
+        ValidateResultObject expectedObj = new ValidateResultObject(false, "Must be a valid day");
+
+        ValidateResultObject returnObj = validate.day(day, month, year);
+
+        assertEquals(expectedObj.ispassing(), returnObj.ispassing());
+        assertEquals(expectedObj.getMessage(), returnObj.getMessage());
+    }
+
+    @Test
+    public void day_passInDayGreaterThanAllowedInMonth() {
+        String day = "30";
+        String month = "2";
+        String year = "2023";
+
+        ValidateResultObject expectedObj = new ValidateResultObject(false, "Must be a valid day");
+
+        ValidateResultObject returnObj = validate.day(day, month, year);
+
+        assertEquals(expectedObj.ispassing(), returnObj.ispassing());
+        assertEquals(expectedObj.getMessage(), returnObj.getMessage());
+    }
+
+    @Test
+    public void day_passInExtraDayInNonLeapYear() {
+        String day = "29";
+        String month = "2";
+        String year = "2023";
+
+        ValidateResultObject expectedObj = new ValidateResultObject(false, "Must be a valid day");
 
         ValidateResultObject returnObj = validate.day(day, month, year);
 
