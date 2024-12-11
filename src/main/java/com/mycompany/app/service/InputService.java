87c3e028f455aa1997575b2ab12146e693949d30
Osvaldo Mesa <d3v.ozzy@gmail.com>
2024-12-10 17:38:06 -0800

    update date sent from InputService

--- src/main/java/com/mycompany/app/service/InputService.java
+++ src/main/java/com/mycompany/app/service/InputService.java
@@ -7,23 +7,24 @@ import com.mycompany.app.model.ValidateResultObject;
 import com.mycompany.app.util.Validate;
 
 public class InputService {
-    private Validate validator = new Validate();
+  private Validate validator = new Validate();
 
-    public InputService() {
-    }
+  public InputService() {
+  }
 
-    // TODO: return to controller data relevate for ResponseInput View
-    public Map<String, ValidateResultObject> validate(String day, String month, String year) {
-        String sanitizedDay = day.trim();
-        String sanitizedMonth = month.trim();
-        String sanitizedYear = year.trim();
-        // Map<> returnMap = new HashMap<>(Map.of("model", mapModel, "passing"));
+  public Map<String, ValidateResultObject> validate(String day, String month, String year) {
+    String sanitizedDay = day.trim();
+    String sanitizedMonth = month.trim();
+    String sanitizedYear = year.trim();
 
-        Map<String, ValidateResultObject> validationResultMap = new HashMap<String, ValidateResultObject>();
+    Map<String, ValidateResultObject> validationResultMap = new HashMap<String, ValidateResultObject>();
 
-        ValidateResultObject dayResult = validator.day(sanitizedDay, sanitizedMonth, sanitizedYear);
+    ValidateResultObject dayResult = validator.day(sanitizedDay, sanitizedMonth, sanitizedYear);
+    // TODO: conduct month and year tests, return put objects on Map
 
-        validationResultMap.put("dayResult", dayResult);
-        return validationResultMap;
-    }
+    validationResultMap.put("dayResultObject", dayResult);
+    validationResultMap.put("monthResultObject", new ValidateResultObject(true, ""));
+    validationResultMap.put("yearResultObject", new ValidateResultObject(true, ""));
+    return validationResultMap;
+  }
 }
