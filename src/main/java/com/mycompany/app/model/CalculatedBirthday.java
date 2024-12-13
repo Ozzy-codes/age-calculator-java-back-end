2c2089a12abe6d129bb61269b6cf871e2a28ffc9
Osvaldo Mesa <d3v.ozzy@gmail.com>
2024-12-13 11:40:46 -0800

    init CalculatedBirthday model, and CalculateBirthday utility class

---
+++ src/main/java/com/mycompany/app/model/CalculatedBirthday.java
@@ -0,0 +1,40 @@
+package com.mycompany.app.model;
+
+public class CalculatedBirthday {
+  private Integer year;
+  private Integer month;
+  private Integer day;
+
+  public CalculatedBirthday() {
+  }
+
+  public CalculatedBirthday(Integer year, Integer month, Integer day) {
+    this.year = year;
+    this.month = month;
+    this.day = day;
+  }
+
+  public Integer getYear() {
+    return year;
+  }
+
+  public void setYear(Integer year) {
+    this.year = year;
+  }
+
+  public Integer getMonth() {
+    return month;
+  }
+
+  public void setMonth(Integer month) {
+    this.month = month;
+  }
+
+  public Integer getDay() {
+    return day;
+  }
+
+  public void setDay(Integer day) {
+    this.day = day;
+  }
+}
---
+++ src/main/java/com/mycompany/app/util/CalculateBirthday.java
@@ -0,0 +1,36 @@
+package com.mycompany.app.util;
+
+import java.util.Calendar;
+
+import com.mycompany.app.model.CalculatedBirthday;
+
+public class CalculateBirthday {
+
+  public CalculatedBirthday getAge(Integer day, Integer month, Integer year) {
+    CalculatedBirthday birthday = new CalculatedBirthday();
+    Calendar currentDate = Calendar.getInstance();
+    Integer currentYear = currentDate.get(Calendar.YEAR);
+    Integer currentMonth = currentDate.get(Calendar.MONTH);
+    Integer currentDay = currentDate.get(Calendar.DATE);
+
+    Integer monthDifference = month - currentMonth;
+    Integer dayDifference = day - currentDay;
+
+    if (monthDifference == 0) {
+      if (dayDifference > 0) {
+        birthday.setYear(currentYear - year - 1);
+        birthday.setMonth(11);
+
+        currentDate.set(currentYear, currentMonth, 0);
+        birthday.setDay(currentDate.getActualMaximum(Calendar.DATE) - day + currentDay);
+      } else {
+        birthday.setYear(currentYear - year);
+        birthday.setMonth(0);
+        birthday.setDay(currentDay - day);
+      }
+      return birthday;
+    }
+
+    return birthday;
+  }
+}
